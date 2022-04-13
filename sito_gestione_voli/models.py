1cbf22b7dbdb5f41cc4838402b051114e3286619
Davide <davidedg97@gmail.com>
2022-04-13 13:02:29 +0200

    secondo commi

--- sito_gestione_voli/models.py
+++ sito_gestione_voli/models.py
@@ -5,50 +5,8 @@ from django.db import models
 
 # Create your models here.
 
-
-
-
-'''
-class prenotazione:
-    andata = models.CharField(max_length=200)
-    ritorno = models.CharField(max_length=200)
-    date_andate = models.DateField() #assicurati che si scriva così
-    date_ritorno = models.DateField()
-    neonati = models.SmallIntegerField()
-    bambini = models.SmallIntegerField()
-    adulti = models.SmallIntegerField()
-    
-class volo:
-    personale = JsonField... #json (tizio caio muzio)
-    posti totali
-    posti disponibili
-
-
-class immagini_sito(models.Model):
-    sfondo = models.ImageField()
-    logo = models.ImageField()
-    
-Class Personale
-    nome = models.CharField(max_length=30)
-    cognome = models.CharField(max_length=30)
-    sesso = models.CharField(max_length=20)
-    id ruolo = models.CharField(max_length=100) # l'id è il ruolo, così colleghi tutto il personale
-    luogo = models.CharField(max_length=200)
-
-Class Ruolo:
-    Assistente = models.CharField(max_length=100)
-    Pilota = models.CharField(max_length=100)
-    ass_Pilota = models.CharField(max_length=100)
-    Hostess_di_volo = models.CharField(max_length=100)
-    Hostess_di_terra = models.CharField(max_length=100)
-    ass_airport = models.CharField(max_length=100)
-    Addetto_di_Scalo = models.CharField(max_length=100) #quelli che vendono i biglietti fisicamente
-    Steward_terra = models.CharField(max_length=100)
-    
-
-
 class Airport(models.Model): 
-    personale = JsonField... #json (tizio caio muzio)
+    #personale = JsonField #json (tizio caio muzio)
     ident = models.CharField(max_length=200)
     ttpe = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
@@ -62,6 +20,37 @@ class Airport(models.Model):
     local_code = models.CharField(max_length=200)
     coordinates = models.CharField(max_length=200)
 
+class Fly:
+    
 
-'''
-#{%load 'sito_gestione_voli/sfondo.gif' static%}
+class Aircraft:
+    name = models.CharField(max_length=200)
+    locate = models.CharField(max_length=300)
+    country = models.CharField(max_length=100)
+    region = models.CharField(max_length=50)
+
+class prenotazione(models.Model):
+    andata = models.ForeignKey(Airport, on_delete=models.SET_NULL, null=True, relative_name = "start")
+    ritorno = models.CharField(Airport, on_delete=models.SET_NULL, null=True, relative_name = "end")
+    date_andate = models.DateField() #assicurati che si scriva così
+    date_ritorno = models.DateField()
+    neonati = models.SmallIntegerField()
+    bambini = models.SmallIntegerField()
+    adulti = models.SmallIntegerField()
+
+class Ruolo(models.Model):
+    Assistente = models.CharField(max_length=100)
+    Pilota = models.CharField(max_length=100)
+    ass_Pilota = models.CharField(max_length=100)
+    Hostess_di_volo = models.CharField(max_length=100)
+    Hostess_di_terra = models.CharField(max_length=100)
+    ass_airport = models.CharField(max_length=100)
+    Addetto_di_Scalo = models.CharField(max_length=100) #quelli che vendono i biglietti fisicamente
+    Steward_terra = models.CharField(max_length=100)
+    
+class Personale(models.Model):
+    nome = models.CharField(max_length=30)
+    cognome = models.CharField(max_length=30)
+    sesso = models.CharField(max_length=20)
+    ruolo = models.CharField(Ruolo, on_delete=models.SET_NULL, null=True)
+    luogo = models.CharField(max_length=200)
